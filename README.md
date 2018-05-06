eb77eb60ea705db94b498b0b06a19cfb71f8fdd2
mchalupa3 <mchalupa3@gmail.com>
2018-05-06 15:53:22 -0500

    beginning

---
+++ README.md
@@ -0,0 +1,1 @@
+# SurvivalV1
---
+++ SurvivalV1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SurvivalV1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Surviaval");
+            Console.WriteLine("The goal of the game is to see how long you can survive.");
+            Console.WriteLine("Press any button to begin the game.");
+        }
+    }
+}
