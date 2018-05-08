9fd319c48c9d6408e13196f72a3dbc8c6e8b5113
Michael Chalupa <mchalupa3@gmail.com>
2018-05-08 10:02:53 -0500

    udpate

--- SurvivalV1/Program.cs
+++ SurvivalV1/Program.cs
@@ -16,6 +16,10 @@ namespace SurvivalV1
         static int food = 5;
         static bool continueGame = true;
         static double userHealth;
+        static int userWood;
+        static Random rnd = new Random();
+        static bool woodenAxe = false;
+
 
 
 
@@ -27,64 +31,73 @@ namespace SurvivalV1
             Console.WriteLine("Welcome to Surviaval");
             Console.WriteLine("The goal of the game is to see how long you can survive.");
             Console.WriteLine("You wake up in the middle of the woods. You need to collect food and water to survive.");
+            Console.WriteLine("You should probably gather wood and build a little shack to live in.");
+            Console.WriteLine("Make sure to keep an eye on your vitals!");
             Console.WriteLine("Type *commands* to get a list of commands.");
             while (continueGame == true)
             {
                 userFood = 50;
-                userWater = 100;
+                userWater = 50;
                 userHealth = 100;
                 while (userHealth > 0 && userFood > 0 && userWater > 0)
                 {
 
-
-
-
-                    while (continueGame == true)
-                    {
-
-
-                        input = Console.ReadLine();
-                        input = input.ToLower();
-                        if (input == "commands")
-                            commands();
-                        if (input == "inventory")
-                        {
-                            inventory();
-                        }
-                        if (input == "drink water")
-                            drinkWater();
-                        if (input == "vitals")
-                            vitals();
-                        if (input == "eat")
-                            eat();
-                    }
-                    
+                    input = Console.ReadLine();
+                    input = input.ToLower();
+                    if(userWater <= 25 || userFood <= 25)
+                        Console.WriteLine("Your vitals are kinda low, might wanna check on that.");
+                    if (input == "commands")
+                        commands();
+                    if (input == "inventory")
+                        inventory();
+                    if (input == "drink")
+                        drinkWater();
+                    if (input == "vitals")
+                        vitals();
+                    if (input == "eat")
+                        eat();
+                    if (input == "death")
+                        death();
+                    if (input == "build")
+                        build();
+                    if (input == "gather wood")
+                        gatherWood();
+                    if (input == "clear")
+                        consoleClear();
+                    if (input == "gather water")
+                        gatherWater();
+                    if (input == "gather food")
+                        gatherFood();
 
                 }
 
 
-                Console.WriteLine("You died");
+                Console.WriteLine("You died.");
                 continueGame = false;
             }
 
 
         }
-        public static void commands()
+        public static void commands() //lists commands 
         {
-            Console.WriteLine("To view your inventory: inventory");
-            Console.WriteLine("To check your vitals: vitals");
-            Console.WriteLine("To check the time: time");
+            Console.WriteLine("To view your inventory: inventory                           To get wood: gather wood");
+            Console.WriteLine("To check your vitals: vitals                                To get more water: collect water");
+            Console.WriteLine("To check the time: time                                     To clear the console: clear");
+            Console.WriteLine("To eat food: eat");
+            Console.WriteLine("To drink water: drink");
+            Console.WriteLine("To build: build");
 
         }
 
-        public static void inventory()
+        public static void inventory() // displays inventory 
         {
             Console.WriteLine("Water Bottles left: " + waterBottles);
             Console.WriteLine("Food Left: " + food);
+            Console.WriteLine("Wood: " + userWood);
 
         }
 
-        public static void drinkWater()
+        public static void drinkWater() //drinking water
         {
             if (waterBottles == 0)
                 Console.WriteLine("You have no water left!");
@@ -98,7 +111,7 @@ namespace SurvivalV1
                 {
                     waterBottles = waterBottles - 1;
                     userWater = userWater + 10;
-                    Console.WriteLine("Your water level was increased to " + userWater + "and you have " + waterBottles + "water bottles left.");
+                    Console.WriteLine("Your water level was increased to " + userWater + " and you have " + waterBottles + " water bottles left.");
 
                 }
             }
@@ -107,23 +120,23 @@ namespace SurvivalV1
         }
         public static void death()
         {
-            userFood = userFood - 3;
-            userWater = userWater - 3;
+            userFood = userFood - 100;
+            userWater = userWater - 100;
         }
 
-        public static void vitals()
+        public static void vitals() //check vitals
         {
             Console.WriteLine("Water level: " + userWater);
-            Console.WriteLine("Food level: "+ userFood);
+            Console.WriteLine("Food level: " + userFood);
         }
 
-        public static void eat()
+        public static void eat()  //Methood for eating food 
         {
-            if(food == 0)
+            if (food == 0)
                 Console.WriteLine("You have no food left!");
             else
             {
-                if(userFood >= 100)
+                if (userFood >= 100)
                 {
                     Console.WriteLine("Your food level is full.");
                 }
@@ -131,14 +144,101 @@ namespace SurvivalV1
                 {
                     food = food - 1;
                     userFood = userFood + 7;
-                    Console.WriteLine("Your food level was increased to " + userFood + "and you have " + food + "food left.");
+                    Console.WriteLine("Your food level was increased to " + userFood + " and you have " + food + " food left.");
 
                 }
             }
         }
+        public static void travel()
+        {
+
+        }
+
+        public static void build()
+
+        {
+            string input;
+            Console.WriteLine("Welcome to the build menu. Use *exit* to exit the menu.");
+            Console.WriteLine("What would you like to build?");
+            Console.WriteLine("Small TeePee: 25 wood");
+            Console.WriteLine("Wooden Axe: 100 wood:");
+            while(continueGame == true)
+            {
+                input = Console.ReadLine();
+                input = input.ToLower();
+                if(input == "small teepee")
+                {
+                    if(userWood >= 25)
+                    {
+                        userWood = userWood - 25;
+                        Console.WriteLine("You have built a Small TeePee. You have " + userWood + " wood remaining.");
+                    }
+                    else
+                        Console.WriteLine("You do not have enough wood" );
+                }
+                if (input == "exit")
+                {
+                    Console.WriteLine("You have left the build menu");
+                    continueGame = false;
+                }
+                if(input == "wooden axe")
+                {
+                    if(userWood >= 100)
+                    {
+                        userWood = userWood - 100;
+                        Console.WriteLine("You now have a wooden axe!");
+                        woodenAxe = true;
+                    }
+                }
+
+            }
+
+        }
+        public static void gatherWood()
+        {
+            if (woodenAxe == false)
+            {
 
 
+                int woodRand = rnd.Next(4, 8);
+                userWater = userWater - 5;
+                userFood = userFood - 5;
+                userWood = userWood + woodRand;
+                Console.WriteLine("You gathered " + woodRand + " wood.");
+            }
+            else
+            {
+                int woodRand = rnd.Next(10, 12);
+                userWater = userWater - 7;
+                userFood = userFood - 7;
+                userWood = userWood + woodRand;
+                Console.WriteLine("You gathered " + woodRand + " wood.");
 
+            }
+
+        }
+        public static void consoleClear()
+        {
+            Console.Clear();
+        }
+        public static void gatherWater()
+        {
+            waterBottles++;
+            Console.WriteLine("You have filled one bottle of water, you now have " + waterBottles);
+        }
+        public static void gatherFood()
+        {
+            int foodRand = rnd.Next(0, 4);
+            food = food + foodRand;
+            if (foodRand > 1)
+            {
+                Console.WriteLine("You found " + foodRand + " pieces of food and now have " + food + " food.");
+            }
+            else
+                Console.WriteLine("You found " + foodRand + " piece of food and now have " + food + " food.");
+            userWater = userWater -3;
+
+        }
         
 
         
