073689c3d211fb6ce07ca258cdba52c77e4dc73c
Michael Chalupa <mchalupa3@gmail.com>
2018-05-09 23:31:16 -0500

    update

--- SurvivalV1/Program.cs
+++ SurvivalV1/Program.cs
@@ -11,34 +11,37 @@ namespace SurvivalV1
     {
 
         static double userWater;
-        static int waterBottles = 3;
+        static int waterBottles;
         static double userFood;
-        static int food = 5;
+        static int food;
         static bool continueGame = true;
         static double userHealth;
         static int userWood;
         static Random rnd = new Random();
         static bool woodenAxe = false;
-
-
+        static string playerName = "Michael";
+        static int[] saveData;
 
 
 
         public static void Main(string[] args)
         {
             string input;
-
-            Console.WriteLine("Welcome to Surviaval");
+           
+            Console.WriteLine("Welcome to Survival");
             Console.WriteLine("The goal of the game is to see how long you can survive.");
             Console.WriteLine("You wake up in the middle of the woods. You need to collect food and water to survive.");
             Console.WriteLine("You should probably gather wood and build a little shack to live in.");
             Console.WriteLine("Make sure to keep an eye on your vitals!");
             Console.WriteLine("Type *commands* to get a list of commands.");
+                  
             while (continueGame == true)
             {
-                userFood = 50;
-                userWater = 50;
+                userFood = 100;
+                userWater = 100;
                 userHealth = 100;
+
+                Console.WriteLine("Welcome back " + playerName);
                 while (userHealth > 0 && userFood > 0 && userWater > 0)
                 {
 
@@ -68,6 +71,11 @@ namespace SurvivalV1
                         gatherWater();
                     if (input == "gather food")
                         gatherFood();
+                    if (input == "save")
+                        save();
+                    if (input == "load")
+                        loadGame();
+                    
 
                 }
 
@@ -238,6 +246,50 @@ namespace SurvivalV1
                 Console.WriteLine("You found " + foodRand + " piece of food and now have " + food + " food.");
             userWater = userWater -3;
 
+        }
+        public static void save()
+        {
+            try
+            {
+                StreamWriter sw = new StreamWriter(@"C:\Users\mchal\Desktop\test.txt");
+                sw.WriteLine(userFood);
+
+                sw.WriteLine(userWater);
+                sw.WriteLine(userHealth);
+                sw.WriteLine(food);
+                sw.WriteLine(waterBottles);
+                sw.WriteLine(userWood);
+                sw.Close();
+
+            }
+
+            catch(Exception e)
+            {
+                Console.WriteLine("Exception: " + e.Message);
+            }
+
+
+            finally
+            {
+                Console.WriteLine("Your game was saved");
+                
+            }
+        }
+        public static void loadGame()
+        {
+            StreamReader sr = new StreamReader(@"C:\Users\mchal\Desktop\test.txt");
+
+            userWater = Convert.ToInt32(sr.ReadLine());
+            userFood = Convert.ToInt32(sr.ReadLine());
+            userHealth = Convert.ToInt32(sr.ReadLine());
+            food = Convert.ToInt32(sr.ReadLine());
+            waterBottles = Convert.ToInt32(sr.ReadLine());
+            userWood = Convert.ToInt32(sr.ReadLine());
+            userWater = Convert.ToInt32(sr.ReadLine());          
+            sr.Close();
+            Console.WriteLine(userFood);
+            Console.WriteLine(userWater);
+            Console.WriteLine(userHealth);
         }
         
 
